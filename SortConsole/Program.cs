8e453aec665a00ae8d0ff797b4a637cb259450ab
Miroslav Valjašek <miroslav.valjasek@gmail.com>
2019-03-28 10:00:37 +0100

    a

--- SortConsole/Program.cs
+++ SortConsole/Program.cs
@@ -10,6 +10,76 @@ namespace SortConsole
     {
         static void Main(string[] args)
         {
+            int [] data = GenerateData(10000);
+            //BubbleSort(data);                   
+            SelectionSort(data);
+            Console.ReadKey();
+        }
+
+        static void BubbleSort(int[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = 0; j < data.Length - 1; j++)
+                {
+                    if (data[j] > data[j + 1])
+                    {                        
+                        ChangeNumber(ref data[j],ref data[j + 1]);
+                    }
+                }
+                Console.WriteLine((data[(data.Length - 1) - i]));
+            }
+        }
+
+        static void SelectionSort(int[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                int maxIndex = i;
+                for (int j = i; j < data.Length-1;j++)
+                {
+                    if (data[i] > data[j+1])
+                    {
+                        ChangeNumber(ref data[i], ref data[j + 1]);
+                    }
+                }
+                Console.WriteLine(data[i]);
+            }
+        }
+
+        static void InsertSort(int[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                int maxIndex = i;
+                for (int j = i; j < data.Length - 1; j++)
+                {
+                    if (data[i] > data[j + 1])
+                    {
+                        ChangeNumber(ref data[i], ref data[j + 1]);
+                    }
+                }
+                Console.WriteLine(data[i]);
+            }
+        }
+
+        static int[] GenerateData(int size)
+        {
+            int[] data = new int[size];
+            Random r = new Random();
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = r.Next(0, 100000);
+            }
+            return data;
+        }
+
+        static void ChangeNumber(ref int firstNumber, ref int secondNUmber)
+        {
+            
+            int temp = secondNUmber;
+            secondNUmber = firstNumber;
+            firstNumber = temp;
         }
     }
 }
